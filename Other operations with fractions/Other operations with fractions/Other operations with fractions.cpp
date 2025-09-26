f90123d2399cf2ebb10580e050f3f6eba7aa7f7e
Forlirn <mmifincyhb@gmail.com>
2025-09-26 20:48:57 +0500

    Commit

---
+++ Other operations with fractions/Other operations with fractions/Other operations with fractions.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	Fraction operator + (const Fraction& f)
+	{
+		return numerator * f. 
+	}
+
+};
+
+int main()
+{
+	
+
+	return 0;
+}
